b8313c68cc602d5557fa5901590108ed44e3e53d
yyh-gl <yhonda.95.gl@gmail.com>
2020-08-03 22:48:29 +0900

    ろうそく足情報取得処理を実装

--- akindo.go
+++ akindo.go
@@ -37,3 +37,27 @@ func (ac AkindoClient) GetAccount() (string, error) {
 	}
 	return string(b), nil
 }
+
+func (ac AkindoClient) GetCandle() (string, error) {
+	url := "https://api-fxpractice.oanda.com/v3/accounts/101-009-15951441-001/candles/latest?candleSpecifications=USD_JPY:M1:BM"
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodGet, url, nil)
+	if err != nil {
+		return "", fmt.Errorf("リクエスト作成に失敗: %w", err)
+	}
+
+	accessToken := os.Getenv("API_ACCESS_TOKEN")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+
+	c := http.DefaultClient
+	resp, err := c.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("リクエスト実行に失敗: %w", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("レスポンス読込に失敗: %w", err)
+	}
+	return string(b), nil
+}
--- akindo_test.go
+++ akindo_test.go
@@ -14,4 +14,13 @@ func TestAkindo(t *testing.T) {
 		assert.Equal(t, `{"accounts":[{"id":"101-009-15951441-001","tags":[]}]}`, a)
 		assert.Nil(t, err)
 	})
+
+	t.Run("ろうそく足情報を取得できること", func(t *testing.T) {
+		ac := NewAkindoClient()
+		cs, err := ac.GetCandle()
+
+		// ろうそく足情報はリアルタイムに変わっていくので、空でないことを確認
+		assert.NotEmpty(t, cs)
+		assert.Nil(t, err)
+	})
 }
