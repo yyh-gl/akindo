37fc5fa766b73008048c225a638e6e7b9a64fbaf
yyh-gl <yhonda.95.gl@gmail.com>
2020-08-07 00:56:52 +0900

    Akindo関連の基本的な処理実装＆テスト追加

---
+++ akindo.go
@@ -0,0 +1,24 @@
+package main
+
+// akindo : 商売人を表す構造体
+type akindo struct {
+	*oandaClient
+	instrument string
+}
+
+// newAkindo : 商売人召喚
+func newAkindo(oc *oandaClient, instrument string) (*akindo, error) {
+	return &akindo{
+		oandaClient: oc,
+		instrument:  instrument,
+	}, nil
+}
+
+// check : 価格変動を確認
+func (a akindo) check() {}
+
+// buy : 購入
+func (a akindo) buy() {}
+
+// sell : 売却
+func (a akindo) sell() {}
---
+++ akindo_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAkindo(t *testing.T) {
+	var at = os.Getenv("API_ACCESS_TOKEN")
+	var id = os.Getenv("ACCOUNT_ID")
+
+	t.Run("商売人を取得できる", func(t *testing.T) {
+		oc, _ := newOANDAClient(at, id)
+		a, err := newAkindo(oc, "USD_JPY")
+
+		assert.NotNil(t, a)
+		assert.Nil(t, err)
+	})
+
+	t.Run("check()仮テスト", func(t *testing.T) {
+		oc, _ := newOANDAClient(at, id)
+		a, _ := newAkindo(oc, "USD_JPY")
+
+		a.check()
+	})
+
+	t.Run("buy()仮テスト", func(t *testing.T) {
+		oc, _ := newOANDAClient(at, id)
+		a, _ := newAkindo(oc, "USD_JPY")
+
+		a.buy()
+	})
+
+	t.Run("sell()仮テスト", func(t *testing.T) {
+		oc, _ := newOANDAClient(at, id)
+		a, _ := newAkindo(oc, "USD_JPY")
+
+		a.sell()
+	})
+}
