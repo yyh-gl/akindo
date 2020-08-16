07feca93a073fb99b42fec99cfe83b2d83a5f488
yyh-gl <yhonda.95.gl@gmail.com>
2020-08-16 14:12:25 +0900

    Buy()とSell()を本実装&sendRequest()でbodyを送れるように修正

--- oanda/oanda.go
+++ oanda/oanda.go
@@ -1,6 +1,7 @@
 package oanda
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -31,13 +32,23 @@ func NewClient(accessToken, accountID string) *Client {
 }
 
 // sendRequest : OANDA APIへのリクエスト用共通処理
-func (c *Client) sendRequest(ctx context.Context, path string) (*http.Response, error) {
+func (c *Client) sendRequest(ctx context.Context, method, path string, body interface{}) (*http.Response, error) {
+	var b []byte
+	if body != nil {
+		var err error
+		b, err = json.Marshal(body)
+		if err != nil {
+			return nil, fmt.Errorf("json.Marshal(Body: %+v) > %w", body, err)
+		}
+	}
+
 	url := host + path
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(ctx, method, url, bytes.NewBuffer(b))
 	if err != nil {
 		return nil, fmt.Errorf("http.NewRequestWithContext(ctx, %s, %s, nil) > %w", http.MethodGet, url, err)
 	}
 	req.Header.Add("Authorization", "Bearer "+c.accessToken)
+	req.Header.Add("Content-Type", "application/json")
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
@@ -48,9 +59,9 @@ func (c *Client) sendRequest(ctx context.Context, path string) (*http.Response, 
 
 // getAccount : アカウント情報を取得
 func (c *Client) getAccount(ctx context.Context) (string, error) {
-	resp, err := c.sendRequest(ctx, "/accounts")
+	resp, err := c.sendRequest(ctx, http.MethodGet, "/accounts", nil)
 	if err != nil {
-		return "", fmt.Errorf("sendRequest(ctx, \"/accounts\") > %w", err)
+		return "", fmt.Errorf("sendRequest(Path: /accounts, Body: nil) > %w", err)
 	}
 	defer func() { _ = resp.Body.Close() }()
 
@@ -89,9 +100,9 @@ func (c *Client) getCandles(ctx context.Context, instrument string) (candle.Cand
 	}
 
 	path := fmt.Sprintf("/accounts/%s/instruments/%s/candles?granularity=M2", c.accountID, instrument)
-	resp, err := c.sendRequest(ctx, path)
+	resp, err := c.sendRequest(ctx, http.MethodGet, path, nil)
 	if err != nil {
-		return nil, fmt.Errorf("sendRequest(ctx, \"%s\") > %w", path, err)
+		return nil, fmt.Errorf("sendRequest(Path: %s, Body: nil) > %w", path, err)
 	}
 	defer func() { _ = resp.Body.Close() }()
 
@@ -152,12 +163,68 @@ func (c *Client) GetLatestCandle(ctx context.Context, instrument string) (*candl
 
 // buy : 外貨を購入
 func (c *Client) Buy(ctx context.Context, instrument string, units int) error {
-	fmt.Println("buy")
+	type request struct {
+		Order struct {
+			Units        string `json:"units"`
+			Instrument   string `json:"instrument"`
+			Type         string `json:"type"`
+			PositionFill string `json:"positionFill"`
+		} `json:"order"`
+	}
+	type response struct {
+		OrderCreateTransaction struct {
+			AccountID    string    `json:"accountID"`
+			BatchID      string    `json:"batchID"`
+			ID           string    `json:"id"`
+			Instrument   string    `json:"instrument"`
+			PositionFill string    `json:"positionFill"`
+			Reason       string    `json:"reason"`
+			Time         time.Time `json:"time"`
+			TimeInForce  string    `json:"timeInForce"`
+			Type         string    `json:"type"`
+			Units        string    `json:"units"`
+			UserID       string    `json:"userID"`
+		} `json:"orderCreateTransaction"`
+		ErrorMessage string `json:"errorMessage"`
+	}
+
+	req := request{
+		Order: struct {
+			Units        string `json:"units"`
+			Instrument   string `json:"instrument"`
+			Type         string `json:"type"`
+			PositionFill string `json:"positionFill"`
+		}{
+			Units:        strconv.Itoa(units),
+			Instrument:   instrument,
+			Type:         "MARKET",
+			PositionFill: "DEFAULT",
+		},
+	}
+	path := fmt.Sprintf("/accounts/%s/orders", c.accountID)
+	resp, err := c.sendRequest(ctx, http.MethodPost, path, &req)
+	if err != nil {
+		return fmt.Errorf("sendRequest(Path: %s, Body: %+v) > %w", path, req, err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("ioutil.ReadAll() > %w", err)
+	}
+
+	var r response
+	if err := json.Unmarshal(b, &r); err != nil {
+		return fmt.Errorf("json.Unmarshal(%s, response{}) > %w", string(b), err)
+	}
+
+	if r.ErrorMessage != "" {
+		return fmt.Errorf("failed to request > %s", r.ErrorMessage)
+	}
 	return nil
 }
 
 // sell : 外貨を売却
 func (c *Client) Sell(ctx context.Context, instrument string, units int) error {
-	fmt.Println("sell")
-	return nil
+	return c.Buy(ctx, instrument, -units)
 }
